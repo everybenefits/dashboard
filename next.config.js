b26fbc085168e167d31ccfdafb0fa3df09e7ea47
Diesan Romero <info@kemuslabs.com>
2022-08-26 16:35:48 -0400

    Done

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  poweredByHeader: false,
+  compress: true,
+  cleanDistDir: true,
+  optimizeFonts: true,
+  trailingSlash: true,
+  generateEtags: true,
+  productionBrowserSourceMaps: true,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
+  httpAgentOptions: {
+    keepAlive: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
