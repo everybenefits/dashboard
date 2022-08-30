14539d975f10bf58e58eeba3084bcf3781e57eac
Diesan Romero <info@kemuslabs.com>
2022-08-30 19:22:10 -0400

    Changing environment variables

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC0epCRrQij1GBaG1sdgDjZUFYcys-SuXg",
-  authDomain: "the-testing-page.firebaseapp.com",
-  projectId: "the-testing-page",
-  storageBucket: "the-testing-page.appspot.com",
-  messagingSenderId: "1062953388660",
-  appId: "1:1062953388660:web:6f5f9c1b2f9f94b60a9f21"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 }
